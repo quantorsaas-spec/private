2d934d7279d4c6a294362d8538984fa5d8749a36
Pavel Yudchenko <quantorsaas@gmail.com>
2026-02-18 20:42:52 +0200

    fix: add Hibernate dialect configuration for tests
    - Add PostgreSQLDialect to main application.yml
    - Change test dialect from PostgreSQL10Dialect to H2Dialect
    - Add @ActiveProfiles("test") to SmokeHealthTest

--- quantor-api/src/test/java/com/quantor/api/SmokeHealthTest.java
+++ quantor-api/src/test/java/com/quantor/api/SmokeHealthTest.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class SmokeHealthTest {
 
     @Autowired
